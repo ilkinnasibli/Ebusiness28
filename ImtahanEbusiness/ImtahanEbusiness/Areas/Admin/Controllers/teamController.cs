ae216732d055803bea429d29baa56d835f149e78
ilkinnasibli <ilkinnasibli@mail.ru>
2023-01-28 03:33:12 +0400

    business

---
+++ ImtahanEbusiness/ImtahanEbusiness/Areas/Admin/Controllers/teamController.cs
@@ -0,0 +1,49 @@
+﻿using Core.Entities;
+using DataAccess.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImtahanEbusiness.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class teamController : Controller
+    {
+        private AppDbContext _appDbContext;
+
+        public teamController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appDbContext.teams);
+        }
+        public  IActionResult Create( )
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(team item)
+        {
+          if(!ModelState.IsValid) return View(item);
+          await _appDbContext.teams.AddAsync(item);
+            await _appDbContext.SaveChangesAsync();
+          return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult>  Detail(int id)
+        { 
+            var model = await _appDbContext.teams.FindAsync(id);
+
+            return View(model);
+        }
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
